2508b28ea2d86516edcfbfca0beaa422d54083d3
Carlos Moro <carlosmoro95@gmail.com>
2019-02-25 19:57:08 +0100

    Dibuja gráfica 3d

--- P1/regresion.py
+++ P1/regresion.py
@@ -1,5 +1,8 @@
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
 from pandas.io.parsers import read_csv
 
 def h(theta, x):
@@ -19,6 +22,7 @@ def main():
 
 	Thetas, coste = descenso_gradiente(x, y, a)
 	dibuja(x[:, -1], y, Thetas)
+	dibuja_3d(x, y)
 
 def descenso_gradiente(x, y, a):
 	m = len(x)
@@ -50,5 +54,30 @@ def dibuja(x, y, theta):
 	x_h = np.hstack([np.ones((23, 1)), x_matriz])
 	plt.plot(x_range, h(theta, x_h))
 	plt.savefig('regresion.png')
+
+def dibuja_3d(x, y):
+	theta0 = np.arange(-5, 5, 0.1)
+	theta1 = np.arange(-5, 5, 0.1)
+	theta0, theta1 = np.meshgrid(theta0, theta1)
+	costes = np.empty_like(theta0)
+	
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+
+	for xi, yi in np.ndindex(theta0.shape):
+		theta = np.array([theta0[xi, yi], theta1[xi, yi]])
+		costes[xi, yi] = coste(theta, x, y)
+	# Plot the surface.
+	surf = ax.plot_surface(theta0, theta1, costes, cmap=cm.coolwarm,
+	                       linewidth=0, antialiased=False)
+
+	# Customize the z axis.
+	ax.zaxis.set_major_locator(LinearLocator(10))
+	ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+	# Add a color bar which maps values to colors.
+	fig.colorbar(surf, shrink=0.5, aspect=5)
+
+	plt.savefig('3d.png')
 
 main()
