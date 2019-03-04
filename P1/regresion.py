cf8ccf6c6d0a5eece2619d3b5f4338f75f856ee9
Carlos Moro <carlosmoro95@gmail.com>
2019-03-04 16:43:35 +0100

    Comentarios añadidos

--- P1/regresion.py
+++ P1/regresion.py
@@ -6,67 +6,68 @@ import numpy as np
 from pandas.io.parsers import read_csv
 
 def h(theta, x):
-	return np.dot(theta, x.T)
+	return np.dot(theta, x.T)  # Hace el producto escalar entre theta y x transpuesta. y=x0*theta0 + x1*theta1
 
 def main():
-	valores = read_csv('ex1data1.csv', header=None).values
-	valores = valores.astype(float)
+	valores = read_csv('ex1data1.csv', header=None).values  # Lee los valores
+	valores = valores.astype(float)  # Los convierte a float
 	m = len(valores)
 
-	v = np.hstack([np.ones((m, 1)), valores])
+	v = np.hstack([np.ones((m, 1)), valores])  # Le añade una columna de unos a las x. Así se puede hacer la función y=theta0 + x*theta1 como un producto escalar
 
-	x = v[:, 0:2]
-	y = v[:, -1:]
+	x = v[:, 0:2]  # Separa la matriz de valores en x
+	y = v[:, -1:]  # e y
 
-	a = 0.01
+	a = 0.01  # Valor alpha de entrenamiento
 
-	Thetas, coste = descenso_gradiente(x, y, a)
-	dibuja(x[:, -1], y, Thetas)
-	theta0, theta1, costes = coste_3d(x, y)
-	dibuja_3d(theta0, theta1, costes)
-	dibuja_contorno(theta0, theta1, costes)
+	Thetas, coste = descenso_gradiente(x, y, a)  # Calculamos el menor coste y con que thetas se obtiene
+	dibuja(x[:, -1], y, Thetas)  # Dibuja la recta de regresión sobre los puntos de los valores
+	theta0, theta1, costes = coste_3d(x, y)  # Calcula todos los costes en forma de malla para dibujar la gráfica en 3D
+	dibuja_3d(theta0, theta1, costes)  # Dibuja la gráfica en 3D
+	dibuja_contorno(theta0, theta1, costes)  # Dibuja la proyección 2D del plano de la 3D.
 
 def descenso_gradiente(x, y, a):
 	m = len(x)
-	theta = np.array([1., 1.])
+	theta = np.array([1., 1.])  # Valor inicial de thetas que luego se irán actualizando
 	costes = []
 	thetas_array = []
 	for i in range(1500):
 		sum0 = 0
 		sum1 = 0
-		for j in range(m):
-			sum0 += h(theta, x[j, :]) - y[j]
-			sum1 += (h(theta, x[j, :]) - y[j]) * x[j, -1:]
-		theta[0] = theta[0] - (a / m) * sum0
-		theta[1] = theta[1] - (a / m) * sum1
-		costes.append(coste(theta, x, y))
-		thetas_array.append(theta)
+		for j in range(m):  # Para cada uno de los valores (filas de x o y):
+			sum0 += h(theta, x[j, :]) - y[j]  # Se le resta el valor real (y) al que da como respuesta la ecuación con las thetas actuales y se suman al sumatorio
+			sum1 += (h(theta, x[j, :]) - y[j]) * x[j, -1:]  # Para theta1, además se multiplica por la variable leída de x. En el caso anterior también, pero como siempre se 1 no lo ponemos
+		theta[0] = theta[0] - (a / m) * sum0  # Se actualiza el valor de theta0 y theta1 con estas ecuaciones.
+		theta[1] = theta[1] - (a / m) * sum1  # MUY IMPORTANTE hacer primero los dos sumatorios antes de actualizar las thetas
+		costes.append(coste(theta, x, y))  # Se calcula el coste y se añade al array de costes
+		thetas_array.append(theta)  # Se añaden las thetas al array con todas las thetas calculadas
 
+	# Se devuelve el coste mínimo y las thetas que lo provocan
 	return (thetas_array[np.argmin(costes)], np.min(costes))
 
 def coste(theta, x, y):
 	m = len(x)
-	coste = (1 / (2 * m)) * np.sum((h(theta, x) - y.T)**2)
+	coste = (1 / (2 * m)) * np.sum((h(theta, x) - y.T)**2)  # Ecuación del coste
 	return coste
 
 def coste_3d(x, y):
 	theta0 = np.arange(-10, 10, 0.1)
 	theta1 = np.arange(-1, 4, 0.1)
-	theta0, theta1 = np.meshgrid(theta0, theta1)
-	costes = np.empty_like(theta0)
+	theta0, theta1 = np.meshgrid(theta0, theta1)  # Convierte los valores de theta0 y theta1 en una malla.
+	costes = np.empty_like(theta0)  # Se crea una matriz costes vacía igual que theta0
 
 	for xi, yi in np.ndindex(theta0.shape):
 		theta = np.array([theta0[xi, yi], theta1[xi, yi]])
 		costes[xi, yi] = coste(theta, x, y)
 
-	return (theta0, theta1, costes)
+	return (theta0, theta1, costes)  # Devuelve las matrices de thetas0, thetas1 y costes
 
 def dibuja(x, y, theta):
-	plt.plot(x, y, 'rx')
+	plt.plot(x, y, 'rx')  # Dibuja los valores como una 'x' roja
 	x_range = range(23)
-	x_matriz = np.array(x_range).reshape(23, 1)
-	x_h = np.hstack([np.ones((23, 1)), x_matriz])
-	plt.plot(x_range, h(theta, x_h))
+	x_matriz = np.array(x_range).reshape(23, 1)  # Convierte la fila en columna
+	x_h = np.hstack([np.ones((23, 1)), x_matriz])  # Le añade una columna de unos
+	plt.plot(x_range, h(theta, x_h))  # Dibuja la recta de regresión
 	plt.savefig('regresion.png')
 
 def dibuja_3d(theta0, theta1, costes):
--- P1/regresion2.py
+++ P1/regresion2.py
@@ -6,85 +6,88 @@ import numpy as np
 from pandas.io.parsers import read_csv
 
 def h(theta, x):
-	return np.dot(x, theta)
+	return np.dot(x, theta)  # Hace el producto escalar entre x y theta. y=x0*theta0 + x1*theta1 + x2*theta2...
 
 def main():
-	valores = read_csv('ex1data2.csv', header=None).values
-	valores = valores.astype(float)
+	valores = read_csv('ex1data2.csv', header=None).values  # Lee los valores
+	valores = valores.astype(float)  # Los convierte a float
 
-	x = valores[:, 0:-1]
-	y = valores[:, -1:]
+	x = valores[:, 0:-1]  # Separa la matriz de valores en x
+	y = valores[:, -1:]  # e y
 
 	a = []
 
-	for i in range(2, 5):
+	# Añade valores al array de los valores alpha de entrenamiento 
+	for i in range(1, 5):
 		a.append(1 / (10**i))
 		a.append(3 / (10**i))
 
-	normalX, mu, sigma = normal(x)
+	normalX, mu, sigma = normal(x)  # Normaliza los valores x y devuelve el valor mu y sigma
 
-	Theta, coste, a = calcA(normalX, y, a)
-	Theta2 = ecuacion_normal(x, y)
+	Theta, coste, a = calcA(normalX, y, a)  # Calcula el mejor valor a, el coste más bajo y las thetas que lo provocan (Se necesita normalizar los valores)
+	Theta2 = ecuacion_normal(x, y)  # Calcula los valores theta con la ecuación normal
 
-	x1 = np.array([1, 1650, 3])
-	x1n = np.array([1, (1650 - mu[0]) / sigma[0], (3 - mu[1]) / sigma[1]])
+	x1 = np.array([1, 1650, 3])  # Genera un nuevo dato
+	x1n = np.array([1, (1650 - mu[0]) / sigma[0], (3 - mu[1]) / sigma[1]])  # Genera un neuvo dato normalizado
 
-	y1 = h(Theta, x1n.T)
-	y2 = h(Theta2, x1.T)
-	print(y1)
-	print(y2)	
+	y1 = h(Theta, x1n.T)  # Calcula el valor de la y con las primeras thetas añadas y el valor normalizado
+	y2 = h(Theta2, x1.T)  # Calcula el valor de la y con las segundas thetas añadas y el valor según llega
+	print('La casa cuesta {:.2f}€ según el método del descenso de gradiente'.format(y1[0]))
+	print('La casa cuesta {:.2f}€ según el método de la ecuación normal'.format(y2[0]))
 
 def ecuacion_normal(x, y):
 	m = len(x)
-	x = np.hstack([np.ones((m, 1)), x])
-	part1 = np.linalg.inv(np.dot(x.T, x))
-	part2 = np.dot(part1, x.T)
-	theta = np.dot(part2, y)
+	x = np.hstack([np.ones((m, 1)), x])  # Añade una columna de unos
+	part1 = np.linalg.inv(np.dot(x.T, x))  # Hace la inversa del producto escalar entre x transpuesta y x
+	part2 = np.dot(part1, x.T)  # Hace el producto escalar entre el valor anterior y x transpuesta
+	theta = np.dot(part2, y)  # Hace el producto escalar entre el valor anterior e y
 	return theta
 
 def normal(x):
 	mu = np.mean(x, axis=0)
 	sigma = np.std(x, axis=0)
-	normalX = (x - mu) / sigma
-	return (normalX, mu, sigma)
+	normalX = (x - mu) / sigma  # Para normalizr los valores resta la media y divide por la sigma
+	return (normalX, mu, sigma)  # Devuelve la matriz normalizada y los valores mu y sigma que se han usado
 
 def descenso_gradiente(normalX, y, a):
 	m = len(normalX)
 	
 	normalX = np.hstack([np.ones((m, 1)), normalX])
-	theta = np.array(np.ones((len(normalX[0])))).reshape(len(normalX[0]), 1)
+	theta = np.array(np.ones((len(normalX[0])))).reshape(len(normalX[0]), 1)  # Genera tantas thetas de valor 1 como variables distintas
 
-	fig = plt.figure()
+	fig = plt.figure()  # Se crea una gráfica que tendrá los costes por cada repetición
 	ax = fig.gca()
 
 	costes = []
 	thetas_array = []
 	for i in range(1500):
 		columns = len(normalX.T)
-		sumat = [0.] * columns
-		for n in range(columns):
-			for j in range(m):
-				sumat[n] += (h(theta, normalX[j]) - y[j]) * normalX[j, n]
-			theta[n] = theta[n] - (a / m) * sumat[n]
-		costeJ = coste(theta, normalX, y)
-		ax.plot(i, costeJ[0][0], 'bx')
-		costes.append(costeJ)
-		thetas_array.append(theta)
+		sumat = [0.] * columns  # Genera un array del tamaño del número de variables donde se le añadirá los sumatorios
+		for n in range(columns):  # Por cada columna (variable)
+			for j in range(m):  # Y por cada fila (valores distintos de cada variable)
+				sumat[n] += (h(theta, normalX[j]) - y[j]) * normalX[j, n]  # Calcula el sumatorio con esta ecuación
+			theta[n] = theta[n] - (a / m) * sumat[n]  # Se actualiza el valor de las thetas con esta ecuación
+		costeJ = coste(theta, normalX, y)  # Se calcula el coste
+		ax.plot(i, costeJ[0][0], 'bx')  # Se añade el punto con el coste
+		costes.append(costeJ)  # Se añade el coste al array de costes
+		thetas_array.append(theta)  # Se añaden las thetas al array
 	plt.savefig('costes{}.png'.format(a))
+	# Devuelve el coste mínimo y las thetas que lo provocan
 	return (thetas_array[np.argmin(costes)], np.min(costes))
 
 def coste(theta, x, y):
 	m = len(x)
-	coste = (1 / (2 * m)) * np.dot((h(theta, x) - y).T, h(theta, x) - y)
+	coste = (1 / (2 * m)) * np.dot((h(theta, x) - y).T, h(theta, x) - y)  # Ecuación del coste (Una matriz por su transpuesta es equivante al cuadrado de cada valor)
 	return coste
 
 def calcA(normalX, y, array_a):
 	thetas_array = []
 	costes_array = []
-	for a in array_a:
-		Theta, coste = descenso_gradiente(normalX, y, a)
+	for a in array_a:  # Por cada valor de alpha que hemos añadido
+		Theta, coste = descenso_gradiente(normalX, y, a)  # Calculamos el menor coste y con que thetas se obtiene
 		thetas_array.append(Theta)
 		costes_array.append(coste)
+	# Devuelve el menor coste entre las a, la propia a y los valores theta que lo provocan
 	return (thetas_array[np.argmin(costes_array)], np.min(costes_array), array_a[np.argmin(costes_array)])
 
 main()
