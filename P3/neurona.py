b88802e2faa716e1c287756dd256d05c77452134
Carlos Moro <carlosmoro95@gmail.com>
2019-04-08 18:26:12 +0200

    Neurona acabada

--- P3/neurona.py
+++ P3/neurona.py
@@ -1,40 +1,16 @@
 from scipy.io  import loadmat
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
-import scipy.optimize as opt
-from sklearn.preprocessing import PolynomialFeatures
-from pandas.io.parsers import read_csv
 
 def g(z):
 	return 1 / (1 + np.exp(-z))
 
-def buildY(y, K):
-	m = len(y)
-	y_aux = np.zeros((m, K))
-	ks = np.eye(K)
+def evaluar(h, y):
+	m = len(h.T)
+	cont = 0
 	for i in range(m):
-		for k in range(K):
-			if (y[i][0] == k + 1):
-				y_aux[i] = ks[k]
-	return y_aux
-
-def coste(h, y, l, theta):
-	K = len(h)
-	m = len(h[0])
-	y = buildY(y, K)
-	L = len(theta)
-	coste1 = 0
-	coste2 = 0
-	for k in range(K):
-		coste1 += -(1 / m) * np.sum(y.T[k] * np.log(h[k]) + (1 - y.T[k]) * np.log(1 - h[k]))
-	for i in range(L):
-		coste2 += (l / (2 * m)) * np.sum(theta[i][1:]**2)
-	coste = coste1 + coste2
-	return coste
-
+		if (np.argmax(h.T[i]) + 1) == y[i, 0]:
+			cont += 1
+	print('Acierta el {}%\n'.format((cont/m)*100))
 
 def main():
 	weights = loadmat('ex3weights.mat')
@@ -51,8 +27,9 @@ def main():
 	# Almacena los datos leídos en X e y
 	l = 0.1
 	
-	h = getH(X, theta1, theta2)
-	cost = coste(h, y, l, THETA)
+	h = getH(X, theta1, theta2)  # Hace la propagación hacia delante
+	evaluar(h, y)
+
 
 def getH(X, theta1, theta2):
 	m = len(X)
